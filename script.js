b9442451a82a6099a9f42d15af6924d72573aa1e
emoryfisher <70530177+emoryfisher@users.noreply.github.com>
2020-09-21 11:10:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var numberEle = document.body.querySelector(".number");
+var overEle = document.body.querySelector(".over");
+var newtwoEle = document.body.querySelector(".newtwo");
+
+var number = 5;
+
+var UserInput = (prompt("Enter the correct number"));
+ 
+   
+if(number !==5 ){
+  newEle.innerHTML = "Enter a new number";}
+
+if(number > 10){
+overEle.innerHTML = "Program has ended";}
+
+if(number < 10){
+  newtwoEle.innerHTML = "Enter a new number again";}
